25daebf92abcfaddfade31da4bf7e1dc5f281f36
Oscar Renderos <oscar.renderosjr@gmail.com>
2020-06-23 21:49:32 -0700

    pro

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,65 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'Oscar Renderos.',
+        headerTagline: [//Line 1 For Header
+                        'Full Stack Developer with a background in Structural Engineering.',
+                        //Line 2 For Header
+                        'Structural Engineer',
+                        //Line 3 For Header
+                        'from Los Angeles'
+    ],
+        //Contact Email
+        contactEmail:'oscar.renderosjr@gmail.com',
+        // Add Your About Text Here
+        abouttext: " ",
+        aboutImage:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.com%2Fpub%2Fdir%2FOscar%2FRenderos&psig=AOvVaw1r1R92vhbwUGRrnVWgfPx1&ust=1593059060326000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCT083NmeoCFQAAAAAdAAAAABAD',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+
+       projects:[
+           {
+            id: 1,
+            title:'Project One', //Project Title - Add Your Project Title Here
+             service:'In or Out', // Add Your Service Type Here
+
+             imageSrc:"https://user-images.githubusercontent.com/56624676/75085702-92c4cd00-54e0-11ea-939b-ade7058be35d.png",
+
+             url:'https://github.com/abautista3712/In-or-Out'
+            },
+            {
+                id: 2,
+                title: 'Project Two',
+                service: 'WeTriva',
+                imageSrc: "https://user-images.githubusercontent.com/58633404/79640912-3e894280-8149-11ea-8c99-90f750516d62.gif",
+                url: 'https://github.com/nickrayj45/WeTrivia'
+            },
+            { 
+                id: 3,
+                title: 'Project Three',
+                service: 'Club House',
+                imageSrc: "https://serene-sea-67556.herokuapp.com/",
+                url: 'https://github.com/AshelyNicole/ReactProject3'
+            },
+            {
+                id: 4,
+                title: 'Project Two',
+                service: 'WeTriva',
+                imageSrc: "https://user-images.githubusercontent.com/58633404/79640912-3e894280-8149-11ea-8c99-90f750516d62.gif",
+                url: 'https://github.com/nickrayj45/WeTrivia'
+           }
+
+  
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/orenderos'},
+            {
+                name: 'Linkin',
+                url: 'https://www.linkedin.com/in/oscar-renderos-jr-9102677a/'
+            },
+
+        ]
+    }
